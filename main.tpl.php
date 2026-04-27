66d695d95c638596a9b54ad7d60b8736ea91affc
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:29:48 +0200

    Add user info display in main.tpl.php

---
+++ main.tpl.php
@@ -0,0 +1,5 @@
+<?php if(isset($_SESSION['login'])): ?>
+    <div class="user-info">
+        Bejelentkezett: <?= $_SESSION['csaladi_nev'] . " " . $_SESSION['uto_nev'] . " (" . $_SESSION['login'] . ")" ?>
+    </div>
+<?php endif; ?>
