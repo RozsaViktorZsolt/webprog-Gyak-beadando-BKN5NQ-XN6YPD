bf63c945bfde3d20482806e047ce6abbad66db82
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:28:04 +0200

    Create fooldal.tpl.php

---
+++ fooldal.tpl.php
@@ -0,0 +1,9 @@
+<video width="320" height="240" controls>
+  <source src="video/sajat_video.mp4" type="video/mp4">
+  A böngésző nem támogatja a videót.
+</video>
+
+<iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/PÉLDA_ID" 
+    frameborder="0" allowfullscreen>
+</iframe>
