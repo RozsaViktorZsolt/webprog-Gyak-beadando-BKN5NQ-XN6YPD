1c1342f7c5106df5d89c8613db50a6ca56f5d638
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:41:18 +0200

    Delete kapcsolat.tpl.php

--- kapcsolat.tpl.php
+++
@@ -1,6 +0,0 @@
-<form id="kapcsolat-form" action="?oldal=kapcsolat_kuld" method="post">
-    Név: <input type="text" name="nev" id="nev"><br>
-    E-mail: <input type="text" name="email" id="email"><br>
-    Üzenet: <textarea name="szoveg" id="szoveg"></textarea><br>
-    <button type="submit">Küldés</button>
-</form>
