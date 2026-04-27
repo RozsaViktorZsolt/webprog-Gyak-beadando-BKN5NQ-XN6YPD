d3415e7ffde068e4c1f3f0fed1f83d473851c6a1
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:47:22 +0200

    Update kapcsolat.tpl.php

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,8 +1,25 @@
-function ellenoriz() {
-    var hiba = "";
-    var email = document.getElementById("email").value;
-    if (email.indexOf("@") == -1) { hiba += "Érvénytelen email!\n"; }
-    // többi mező...
-    if (hiba != "") { alert(hiba); return false; }
+<form action="?oldal=kapcsolat_mentes" method="post" onsubmit="return validateContact();">
+    Név: <input type="text" name="nev" id="nev_id"><br>
+    Email: <input type="text" name="email" id="email_id"><br>
+    Üzenet: <textarea name="szoveg" id="szoveg_id"></textarea><br>
+    <input type="submit" value="Küldés">
+</form>
+
+<script>
+function validateContact() {
+    let nev = document.getElementById('nev_id').value;
+    let email = document.getElementById('email_id').value;
+    let uzenet = document.getElementById('szoveg_id').value;
+    let errors = [];
+
+    if(nev.length < 3) errors.push("A név túl rövid!");
+    if(!email.includes("@")) errors.push("Érvénytelen email cím!");
+    if(uzenet.trim() == "") errors.push("Az üzenet nem lehet üres!");
+
+    if(errors.length > 0) {
+        alert(errors.join("\n"));
+        return false;
+    }
     return true;
 }
+</script>
