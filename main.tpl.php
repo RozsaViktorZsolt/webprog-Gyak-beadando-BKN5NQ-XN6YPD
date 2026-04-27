ec77032703590a055c2df742ccb17b4698b416c9
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:41:01 +0200

    Delete main.tpl.php

--- main.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php if(isset($_SESSION['login'])): ?>
-    <div class="user-info">
-        Bejelentkezett: <?= $_SESSION['csaladi_nev'] . " " . $_SESSION['uto_nev'] . " (" . $_SESSION['login'] . ")" ?>
-    </div>
-<?php endif; ?>
