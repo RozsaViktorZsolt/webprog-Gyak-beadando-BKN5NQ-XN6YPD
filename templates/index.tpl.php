4a1bdb6142fb63d602bf2444db01a2696d5e5bdc
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:52:18 +0200

    Update index.tpl.php

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,8 +1,8 @@
 <header>
     <h1><?= $ablakcim['cim'] ?></h1>
-    <?php if(isset($_SESSION['login'])) { ?>
+    <?php if(isset($_SESSION['login'])) : ?>
         <div class="user-info">
             Bejelentkezett: <?= $_SESSION['csaladi_nev']." ".$_SESSION['uto_nev']." (".$_SESSION['login'].")" ?>
         </div>
-    <?php } ?>
+    <?php endif; ?>
 </header>
