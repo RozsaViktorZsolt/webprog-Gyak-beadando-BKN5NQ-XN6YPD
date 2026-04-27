84e95a1fd291422b684ea1ecd94be1c421fa4ef8
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-04-27 19:47:49 +0200

    Add video and iframe elements to cimlap.tpl.php

---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,7 @@
+<video width="320" height="240" controls muted>
+  <source src="images/video/sajat.mp4" type="video/mp4">
+</video>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/PÉLDA_ID"></iframe>
+
+<iframe src="https://www.google.com/maps/embed?pb=..." width="600" height="450" style="border:0;"></iframe>
