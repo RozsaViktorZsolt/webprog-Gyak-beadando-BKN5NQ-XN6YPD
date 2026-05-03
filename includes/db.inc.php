a512cf1018b672905d5734fb653dea8a10c5c8b7
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 14:32:18 +0200

    Update db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = '';
-$user = '';
-$pass = '';
+$dbname = 'forma1_gyak';
+$user = 'forma_1gyak';
+$pass = 'Kukamatyi1';
 try {
     $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password, array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
