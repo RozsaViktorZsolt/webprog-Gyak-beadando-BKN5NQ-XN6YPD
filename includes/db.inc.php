9067b700e878877b7d4286e9be1483bd6e33f4f4
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-03 14:31:12 +0200

    Clear database credentials in db.inc.php
    Removed database credentials from db.inc.php.

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'nbibeadando';
-$user = 'nbibeadando';
-$pass = 'XN6JPD';
+$dbname = '';
+$user = '';
+$pass = '';
 try {
     $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password, array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
