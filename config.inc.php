5d2f49afac897a5be4ceac7fb9359b8327f5a7e3
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 19:08:09 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,13 +1,12 @@
 <?php
 $oldalak = array(
-    'fooldal' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
-    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
-    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
-    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)), // Itt hiányzott a vessző!
-    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
-    'bejelentkezes' => array('fajl' => 'bejelentkezes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)),
-    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1))
+    'fooldal'      => array('fajl' => 'home', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+    'kepek'        => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1, 1)),
+    'kapcsolat'    => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1, 1)),
+    'crud'         => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1, 1)),
+    'uzenetek'     => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(1, 1)),
+    'beleptet' => array('fajl' => 'beleptet', 'szoveg' => 'Belépés', 'menun' => array(1,0)), 
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0)), 
+    'kijelentkezes' => array('fajl' => 'kilepes', 'szoveg' => 'Kijelentkezés', 'menun' => array(0,1)),
 );
-
-$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 ?>
