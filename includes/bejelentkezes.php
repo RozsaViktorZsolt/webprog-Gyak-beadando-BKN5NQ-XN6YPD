4d77828821a8819d5b75d78aa97849e6ffd5d86b
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-03 13:40:23 +0200

    Fix session variable assignment in bejelentkezes.php

--- includes/bejelentkezes.php
+++ includes/bejelentkezes.php
@@ -1,4 +1,4 @@
-// Amikor sikeres a jelszó ellenőrzés:
+
 $_SESSION['login'] = $felhasznalo['login'];
-$_SESSION['csn'] = $felhasznalo['csaladi_nev']; // Az adatbázisodból jön
-$_SESSION['un'] = $felhasznalo['uto_nev'];      // Az adatbázisodból jön
+$_SESSION['csn'] = $felhasznalo['csaladi_nev']; 
+$_SESSION['un'] = $felhasznalo['uto_nev'];      
