5d2d2e5249dc6848d24b6148bbc068c51ac9bac1
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:20:47 +0200

    Create crud_torol.php

---
+++ includes/crud_torol.php
@@ -0,0 +1,17 @@
+<?php
+// includes/crud_torol.php
+if (isset($_GET['id'])) {
+    $id = (int)$_GET['id'];
+    try {
+        $sql = "DELETE FROM versenyzok WHERE id = :id";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([':id' => $id]);
+        
+        // Törlés után visszaugrunk a CRUD listához
+        header("Location: index.php?oldal=crud");
+        exit;
+    } catch (PDOException $e) {
+        die("Hiba a törlés során: " . $e->getMessage());
+    }
+}
+?>
