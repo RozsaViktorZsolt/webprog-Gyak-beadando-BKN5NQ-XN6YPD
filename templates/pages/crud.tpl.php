74a48afb6773a30f5ac2d08f0b337ba64f32bc63
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:17:16 +0200

    Update crud.tpl.php

--- templates/pages/crud.tpl.php
+++ templates/pages/crud.tpl.php
@@ -1,34 +1,24 @@
-<?php
-// Adatok lekérése
-$stmt = $dbh->query("SELECT id, nev, csapat, eletkor, varos FROM pilotak");
-$pilotak = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
-<div class="crud-container">
-    <a href="index.php?oldal=crud_uj" class="btn-add">Add New</a>
-    <table>
-        <thead>
-            <tr>
-                <th>Név</th>
-                <th>Életkor</th>
-                <th>Csapat</th>
-                <th>Város</th>
-                <th>Műveletek</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($pilotak as $pilota): ?>
-            <tr>
-                <td><?= htmlspecialchars($pilota['nev']) ?></td>
-                <td><?= $pilota['eletkor'] ?></td>
-                <td><?= htmlspecialchars($pilota['csapat']) ?></td>
-                <td><?= htmlspecialchars($pilota['varos']) ?></td>
-                <td>
-                    <a href="index.php?oldal=crud_szerkeszt&id=<?= $pilota['id'] ?>" class="btn-edit">Edit</a>
-                    <a href="index.php?oldal=crud_torol&id=<?= $pilota['id'] ?>" class="btn-delete" onclick="return confirm('Biztosan törlöd?')">Delete</a>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
+<h2>Forma-1 Adatkezelés</h2>
+<a href="index.php?oldal=crud_add" class="btn">Add New</a>
+<table>
+    <tr>
+        <th>Név</th>
+        <th>Életkor</th>
+        <th>Csapat</th>
+        <th>Akciók</th>
+    </tr>
+    <?php
+    $stmt = $dbh->query("SELECT * FROM versenyzok"); // A táblanevet ellenőrizd az SQL-edben!
+    while($sor = $stmt->fetch(PDO::FETCH_ASSOC)):
+    ?>
+    <tr>
+        <td><?= htmlspecialchars($sor['nev']) ?></td>
+        <td><?= $sor['eletkor'] ?></td>
+        <td><?= htmlspecialchars($sor['csapat']) ?></td>
+        <td>
+            <a href="index.php?oldal=crud_edit&id=<?= $sor['id'] ?>">Edit</a>
+            <a href="index.php?oldal=crud_del&id=<?= $sor['id'] ?>">Delete</a>
+        </td>
+    </tr>
+    <?php endwhile; ?>
+</table>
