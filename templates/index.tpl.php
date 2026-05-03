00650804180e301c119b65db8fe0ac9649d07510
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:16:37 +0200

    Update index.tpl.php

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -3,30 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projektmunka</title>
+    <title>Webprog Projekt</title>
     <link rel="stylesheet" href="./css/stilus.css">
 </head>
 <body>
     <header>
-        <?php if(isset($_SESSION['login'])) { ?>
+        <?php if(isset($_SESSION['login'])): ?>
             <p>Bejelentkezett: <?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></p>
-        <?php } ?>
+        <?php endif; ?>
         <nav>
-            <ul>
-                <?php foreach ($oldalak as $ut => $oldal) { 
-                    // Logika a menüpontok megjelenítésére jogosultság szerint
-                    if (($oldal['menun'][0] && !isset($_SESSION['login'])) || 
-                        ($oldal['menun'][1] && isset($_SESSION['login']))) { ?>
-                        <li><a href="index.php?oldal=<?= $ut ?>"><?= $oldal['szoveg'] ?></a></li>
-                <?php } } ?>
+            <ul class="main-menu">
+                <?php foreach ($oldalak as $ut => $old): 
+                    if (($old['menun'][0] && !isset($_SESSION['login'])) || 
+                        ($old['menun'][1] && isset($_SESSION['login']))): ?>
+                        <li><a href="index.php?oldal=<?= $ut ?>"><?= $old['szoveg'] ?></a></li>
+                    <?php endif; 
+                endforeach; ?>
             </ul>
         </nav>
     </header>
-
     <main>
         <?php include("./templates/pages/{$keresett_oldal['fajl']}.tpl.php"); ?>
     </main>
-
-    <footer>&copy; 2026 - Web-programozás 1 Projekt</footer>
 </body>
 </html>
