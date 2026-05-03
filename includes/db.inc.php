7130c7d9ea136359e0c004aab91a12c7525c3449
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 15:46:40 +0200

    Refactor database connection parameters and error handling

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'forma1_gyak';
-$user = 'forma_1gyak';
-$pass = 'Kukamatyi1';
+$host = 'localhost'; 
+$dbname = 'forma1_gyak'; 
+$user = 'forma_1gyak'; 
+$password = 'Kukamatyi1'; 
+
 try {
-    $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password, array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ));
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Hiba az adatbázis-kapcsolatban: " . $e->getMessage());
 }
+?>
