35bfb76b356eb6620c4a2138d8c3b8b5073a9c1b
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 15:15:48 +0200

    Update bejelentkezes.php

--- includes/bejelentkezes.php
+++ includes/bejelentkezes.php
@@ -1,4 +1,6 @@
-
-$_SESSION['login'] = $felhasznalo['login'];
-$_SESSION['csn'] = $felhasznalo['csaladi_nev']; 
-$_SESSION['un'] = $felhasznalo['uto_nev'];      
+if ($jelszo_ok) {
+    $_SESSION['login'] = $user['login_nev'];
+    $_SESSION['csn'] = $user['csaladi_nev']; // Adatbázis 'csaladi_nev' oszlopa
+    $_SESSION['un'] = $user['uto_nev'];      // Adatbázis 'uto_nev' oszlopa
+    header("Location: index.php");
+}
