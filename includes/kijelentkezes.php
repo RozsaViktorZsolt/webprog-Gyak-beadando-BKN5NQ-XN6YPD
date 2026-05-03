584750bf5ca958bb7430e03acec65560862a56ac
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 19:09:26 +0200

    Implement logout functionality with redirect message

--- includes/kijelentkezes.php
+++ includes/kijelentkezes.php
@@ -1,1 +1,13 @@
+<?php
+// Töröljük a munkamenet adatait
+$_SESSION = array();
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-42000, '/');
+}
+session_destroy();
 
+$uzenet = "Sikeresen kijelentkezett!";
+?>
+<h3><?= $uzenet ?></h3>
+<p>Átirányítás a főoldalra...</p>
+<script>setTimeout(function(){ window.location.href = "index.php"; }, 2000);</script>
