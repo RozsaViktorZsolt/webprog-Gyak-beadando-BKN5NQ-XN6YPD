8bc4d2ed9f0298b3ef2f88ce00a496c7e1c91d50
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:16:06 +0200

    Refactor index.php for improved readability

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
+// index.php
 session_start();
 require_once('./includes/db.inc.php');
 require_once('./config.inc.php');
 
-// Meghatározzuk melyik oldalt kell betölteni
 $oldal = isset($_GET['oldal']) ? $_GET['oldal'] : 'fooldal';
 if (!isset($oldalak[$oldal])) {
     $oldal = 'fooldal';
 }
 $keresett_oldal = $oldalak[$oldal];
 
-include('./templates/index.tpl.php'); // Ez rajzolja ki az oldalt
+include('./templates/index.tpl.php');
 ?>
