bba1f4f0b46b50d39d6909b8f182d5fbf0b88e7f
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:04:46 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,2 +1,13 @@
-$dbh = new PDO('mysql:host=localhost;dbname=ADATBÁZIS_NEVE', 'FELHASZNÁLÓNÉV', 'JELSZÓ',
-      array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+<?php
+$oldalak = array(
+    'fooldal' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)), // Csak belépve (0,1)
+    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)), // Csak kijelentkezve
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1))  // Csak bejelentkezve
+);
+
+$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
