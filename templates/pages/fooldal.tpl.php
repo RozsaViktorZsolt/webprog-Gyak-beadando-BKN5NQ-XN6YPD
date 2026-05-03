e22e29e9603e4c7869ce045568f0db48b7f514b7
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:05:29 +0200

    Enhance homepage with video and map embeds
    Added video and map sections to the homepage template.

--- templates/pages/fooldal.tpl.php
+++ templates/pages/fooldal.tpl.php
@@ -1,1 +1,10 @@
-
+<h2>Üdvözöljük a Forma-1 Rajongói Oldalon!</h2>
+<div class="video-container">
+    <video width="320" height="240" autoplay muted>
+        <source src="./video/sajat_video.mp4" type="video/mp4">
+    </video>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEÓ_ID" frameborder="0"></iframe>
+</div>
+<div class="map-container">
+    <iframe src="https://www.google.com/maps/embed?..." width="600" height="450"></iframe>
+</div>
