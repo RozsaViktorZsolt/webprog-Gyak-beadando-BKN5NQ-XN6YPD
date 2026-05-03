6bf55c6b312ed4266c2f7741c08e6823ba20b8d4
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:04:07 +0200

    Add navigation structure and error page handling

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,8 +1,13 @@
-<header>
-    <h1><?= $ablakcim['cim'] ?></h1>
-    <?php if(isset($_SESSION['login'])) : ?>
-        <div class="user-info">
-            Bejelentkezett: <?= $_SESSION['csaladi_nev']." ".$_SESSION['uto_nev']." (".$_SESSION['login'].")" ?>
-        </div>
-    <?php endif; ?>
-</header>
+<?php
+$oldalak = array(
+    'fooldal' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)), // Csak belépve (0,1)
+    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)), // Csak kijelentkezve
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1))  // Csak bejelentkezve
+);
+
+$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
