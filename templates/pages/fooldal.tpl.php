bb3bef8874b42fcac5858106875024fa6ac0551e
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-03 14:11:20 +0200

    Update fooldal.tpl.php

--- templates/pages/fooldal.tpl.php
+++ templates/pages/fooldal.tpl.php
@@ -3,17 +3,14 @@
 
 <div class="media-container">
     <h3>Videók</h3>
-    <!-- Saját videó, max 5 másodperc[cite: 1] -->
     <video width="320" height="240" controls>
         <source src="video/sajat_video.mp4" type="video/mp4">
     </video>
 
-    <!-- YouTube videó[cite: 1] -->
     <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
 </div>
 
 <div class="map-container">
     <h3>Irodánk helyszíne</h3>
-    <!-- Google térkép[cite: 1] -->
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2758.823485093556!2d20.1437142!3d46.2536835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474487e07662c57b%3A0xc07a83d4c3898144!2sSzeged!5e0!3m2!1shu!2shu!4v1700000000000" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </div>
