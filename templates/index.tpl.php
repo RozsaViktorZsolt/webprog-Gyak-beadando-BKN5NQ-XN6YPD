6b7c1c442e2568f9c0c2b37a9343fe80cd99f10d
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:04:20 +0200

    Update index.tpl.php

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,13 +1,32 @@
-<?php
-$oldalak = array(
-    'fooldal' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
-    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
-    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
-    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)), // Csak belépve (0,1)
-    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
-    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)), // Csak kijelentkezve
-    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1))  // Csak bejelentkezve
-);
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projektmunka</title>
+    <link rel="stylesheet" href="./css/stilus.css">
+</head>
+<body>
+    <header>
+        <?php if(isset($_SESSION['login'])) { ?>
+            <p>Bejelentkezett: <?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></p>
+        <?php } ?>
+        <nav>
+            <ul>
+                <?php foreach ($oldalak as $ut => $oldal) { 
+                    // Logika a menüpontok megjelenítésére jogosultság szerint
+                    if (($oldal['menun'][0] && !isset($_SESSION['login'])) || 
+                        ($oldal['menun'][1] && isset($_SESSION['login']))) { ?>
+                        <li><a href="index.php?oldal=<?= $ut ?>"><?= $oldal['szoveg'] ?></a></li>
+                <?php } } ?>
+            </ul>
+        </nav>
+    </header>
 
-$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
-?>
+    <main>
+        <?php include("./templates/pages/{$keresett_oldal['fajl']}.tpl.php"); ?>
+    </main>
+
+    <footer>&copy; 2026 - Web-programozás 1 Projekt</footer>
+</body>
+</html>
