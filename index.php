e64878ed9b0e16fdebeb1955be6c18e8b07ed79d
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:13:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,14 @@
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <title>Web programozás-1 Előadás Házi feladat</title>
-</head>
-<body>
+<?php
+session_start();
+require_once('./includes/db.inc.php');
+require_once('./config.inc.php');
 
-<h1>Web programozás-1 Előadás Házi feladat</h1>
+// Meghatározzuk melyik oldalt kell betölteni
+$oldal = isset($_GET['oldal']) ? $_GET['oldal'] : 'fooldal';
+if (!isset($oldalak[$oldal])) {
+    $oldal = 'fooldal';
+}
+$keresett_oldal = $oldalak[$oldal];
 
-<nav>
-    <javascript.html">JavaScript CRUD</a> |
-    <react.html">React CRUD</a> |
-    <spa.html">SPA</a> |
-    <fetchapi.html">Fetch API</a> |
-    <axios.html">Axios</a> |
-    <oojs.html">OOJS</a>
-</nav>
-
-<p>
-Ez az alkalmazás a Web-programozás 1 tantárgy beadandó feladata.
-Bemutatja a JavaScript, React, CRUD műveletek, Fetch API, Axios
-és objektumorientált JavaScript használatát.
-</p>
-
-<footer>
-    <p>Készítette: Vass Zoltán Mihály – XN6JPD</p>
-    <p>Készítette: Rózsa Viktor – BKN5NQ</p>
-</footer>
-
-</body>
-</html>
+include('./templates/index.tpl.php'); // Ez rajzolja ki az oldalt
+?>
