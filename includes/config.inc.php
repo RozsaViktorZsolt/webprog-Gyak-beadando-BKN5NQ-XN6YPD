312ffe32eca79e2988dc770777c08e3c09c3933e
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-03 13:45:33 +0200

    Add configuration file for site settings

---
+++ includes/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Forma-1 NBI Adatbázis',
+);
+
+$oldalak = array(
+    'home' => array('fajl' => 'home', 'szoveg' => 'Főoldal', 'menuben' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menuben' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menuben' => array(1,1)),
+    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menuben' => array(1,1)),
+    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menuben' => array(0,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Bejelentkezés', 'menuben' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kijelentkezés', 'menuben' => array(0,1))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
