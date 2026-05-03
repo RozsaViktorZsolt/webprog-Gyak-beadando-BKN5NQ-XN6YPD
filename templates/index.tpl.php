b186753c9c7185106ac1d17cb2faeb8a04118bb7
xn6jpd <xn6jpd@hallgato.nje.hu>
2026-05-03 13:24:44 +0200

    Update index.tpl.php

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,29 +1,42 @@
+<!-- templates/index.tpl.php -->
 <!DOCTYPE html>
 <html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webprog Projekt</title>
+    <title>Webprog Projekt - BKN5NQ, XN6YPD</title>
     <link rel="stylesheet" href="./css/stilus.css">
 </head>
 <body>
     <header>
-        <?php if(isset($_SESSION['login'])): ?>
-            <p>Bejelentkezett: <?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></p>
-        <?php endif; ?>
+        <div class="user-info">
+            <?php if(isset($_SESSION['login'])): ?>
+                <!-- Előírt formátum: Családi_név Utónév (Login_név)[cite: 1] -->
+                <p>Bejelentkezett: <?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></p>
+            <?php endif; ?>
+        </div>
         <nav>
-            <ul class="main-menu">
-                <?php foreach ($oldalak as $ut => $old): 
-                    if (($old['menun'][0] && !isset($_SESSION['login'])) || 
-                        ($old['menun'][1] && isset($_SESSION['login']))): ?>
-                        <li><a href="index.php?oldal=<?= $ut ?>"><?= $old['szoveg'] ?></a></li>
+            <ul class="nav-list">
+                <?php foreach ($oldalak as $ut => $adat): 
+                    // Menüpontok szűrése jogosultság alapján[cite: 1]
+                    if (($adat['menun'][0] && !isset($_SESSION['login'])) || 
+                        ($adat['menun'][1] && isset($_SESSION['login']))): ?>
+                        <li><a href="index.php?oldal=<?= $ut ?>" <?= ($oldal == $ut) ? 'class="active"' : '' ?>><?= $adat['szoveg'] ?></a></li>
                     <?php endif; 
                 endforeach; ?>
             </ul>
         </nav>
     </header>
+
     <main>
-        <?php include("./templates/pages/{$keresett_oldal['fajl']}.tpl.php"); ?>
+        <?php 
+            $fajl = "./templates/pages/{$keresett_oldal['fajl']}.tpl.php";
+            if(file_exists($fajl)) include($fajl); 
+        ?>
     </main>
+
+    <footer>
+        <p>&copy; 2026 - Készítette: Rózsa Viktor Zsolt & Vass Zoltán</p>
+    </footer>
 </body>
 </html>
