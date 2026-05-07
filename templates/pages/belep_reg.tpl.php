43463ca8a03238eac31687eca3c81f03ae542278
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-07 21:43:56 +0200

    Update belep_reg.tpl.php

--- templates/pages/belep_reg.tpl.php
+++ templates/pages/belep_reg.tpl.php
@@ -1,23 +1,21 @@
-<div class="auth-wrapper">
-    <section class="login-section">
+<div class="auth-container">
+    <div class="login-box">
         <h2>Bejelentkezés</h2>
         <form action="?oldal=beleptet" method="post">
-            <input type="text" name="user" placeholder="Felhasználónév" required>
-            <input type="password" name="pw" placeholder="Jelszó" required>
+            <input type="text" name="user" placeholder="Felhasználónév" required><br>
+            <input type="password" name="pw" placeholder="Jelszó" required><br>
             <button type="submit">Belépés</button>
         </form>
-    </section>
-
+    </div>
     <hr>
-
-    <section class="register-section">
+    <div class="register-box">
         <h2>Regisztráció</h2>
         <form action="?oldal=regisztral" method="post">
-            <input type="text" name="csnev" placeholder="Családi név" required>
-            <input type="text" name="unev" placeholder="Utónév" required>
-            <input type="text" name="user" placeholder="Felhasználónév" required>
-            <input type="password" name="pw" placeholder="Jelszó" required>
+            <input type="text" name="csnev" placeholder="Családi név" required><br>
+            <input type="text" name="unev" placeholder="Utónév" required><br>
+            <input type="text" name="user" placeholder="Felhasználónév" required><br>
+            <input type="password" name="pw" placeholder="Jelszó" required><br>
             <button type="submit">Regisztráció</button>
         </form>
-    </section>
+    </div>
 </div>
