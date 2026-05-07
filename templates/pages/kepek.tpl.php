6ad10ddf610c88143363ca1f549c92323e54b381
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-07 22:46:41 +0200

    Update kepek.tpl.php

--- templates/pages/kepek.tpl.php
+++ templates/pages/kepek.tpl.php
@@ -1,10 +1,8 @@
 <?php
-
-    $MAPPA = './kepek/';
+    $MAPPA = './images/';
     $TIPUSOK = array ('.jpg', '.png', '.jpeg');
     $MEDIATIPUSOK = array('image/jpeg', 'image/png');
     $uzenet = array();
-
 
     if (isset($_SESSION['login']) && isset($_FILES['fajlok'])) {
         foreach($_FILES['fajlok']['name'] as $k => $nev) {
@@ -28,7 +26,6 @@
         }
     }
 
-    
     $kepek = array();
     $olvaso = opendir($MAPPA);
     while (($fajl = readdir($olvaso)) !== false) {
@@ -40,42 +37,5 @@
         }
     }
     closedir($olvaso);
-    arsort($kepek); // Időrendben csökkenő sorrend
+    arsort($kepek);
 ?>
-
-<section class="gallery-container">
-    <h2>F1 Képgaléria</h2>
-
-    <?php if (isset($_SESSION['login'])) : ?>
-        <div class="upload-form">
-            <h3>Új kép feltöltése</h3>
-            <form action="?oldal=kepek" method="post" enctype="multipart/form-data">
-                <input type="file" name="fajlok[]" accept="image/*" multiple required>
-                <button type="submit">Képek feltöltése</button>
-            </form>
-            <div class="messages">
-                <?php foreach($uzenet as $u) echo "<p>$u</p>"; ?>
-            </div>
-        </div>
-    <?php else : ?>
-        <p class="info-msg"><i>Jelentkezzen be, ha szeretne képeket feltölteni!</i></p>
-    <?php endif; ?>
-
-    <div class="gallery-grid">
-        <?php if(empty($kepek)): ?>
-            <p>Még nincsenek képek a galériában.</p>
-        <?php else: ?>
-            <?php foreach($kepek as $fajl => $datum): ?>
-                <div class="gallery-item">
-                    <a href="<?= $MAPPA.$fajl ?>" target="_blank">
-                        <img src="<?= $MAPPA.$fajl ?>" alt="<?= $fajl ?>">
-                    </a>
-                    <div class="item-info">
-                        <span class="filename"><?= $fajl ?></span><br>
-                        <span class="date"><?= date("Y-m-d H:i", $datum) ?></span>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
-</section>
