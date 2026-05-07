0cf3be19b3b59e2659cfae9b7111c0acbbe6a36d
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-05-07 21:41:48 +0200

    Update navigation array with new entries

--- config.inc.php
+++ config.inc.php
@@ -1,12 +1,13 @@
 <?php
 $oldalak = array(
     'fooldal'      => array('fajl' => 'home', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
-    'kepek'        => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1, 1)),
-    'kapcsolat'    => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1, 1)),
-    'crud'         => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1, 1)),
-    'uzenetek'     => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(1, 1)),
-    'beleptet' => array('fajl' => 'beleptet', 'szoveg' => 'Belépés', 'menun' => array(1,0)), 
-    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0)), 
+    'kepek'        => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+    'kapcsolat'    => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'crud'         => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
+    'uzenetek'     => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+    'bejelentkezes' => array('fajl' => 'belepes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)), 
+    'beleptet'     => array('fajl' => 'beleptet', 'szoveg' => '', 'menun' => array(0,0)), 
+    'regisztral'   => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0)), 
     'kijelentkezes' => array('fajl' => 'kilepes', 'szoveg' => 'Kijelentkezés', 'menun' => array(0,1)),
 );
 ?>
